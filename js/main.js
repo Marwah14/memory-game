221879bbdfd2ac4be3c4159063f55da14ffb8396
Marwah <memo_224@live.com>
2018-11-05 12:02:10 +0300

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,20 +2,24 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = [0];
-cardsInPlay.push('cardOne');
-console.log("User flipped queen");
-var cardTwo = [2];
-cardsInPlay.push('cardTwo')
-console.log ("User flipped King");
-
-if (cardsInPlay.lenght === 2) {
-	if (cardsInPlay[0] === cardsInPlay[2]) { 
-		alert("You found a match!");
+var checkForMatch = () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("you found a match!");
 	} else {
-		alert("Sorry, try again");
+		alert("sorry, try again.");
 	};
-};
+}
+
+var flipCard = function (cardId) {
+	console.log(" User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+	};
+	
+	
+flipCard(0);
+flipCard(1);
 
 
 
